117932087da8c01e8181de247115ca4ee5004b42
Frédéric Notet <frederic@musimap.com>
2020-06-24 13:51:02 +0100

    fix description

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@
 #################
 resource "aws_ec2_transit_gateway" "this" {
 
-  description                     = "coalesce(var.description, var.name)"
+  description                     = coalesce(var.description, var.name)
   amazon_side_asn                 = var.amazon_side_asn
   default_route_table_association = var.enable_default_route_table_association ? "enable" : "disable"
   default_route_table_propagation = var.enable_default_route_table_propagation ? "enable" : "disable"
